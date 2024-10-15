f3ed5e50114cef7c9fb1c75306a796a05c3866c0
GarrethD <54701909+GarrethD@users.noreply.github.com>
2024-10-15 21:07:13 +0200

    Fixed a bunch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,11 @@
-﻿particlesJS.load('particles-js', 'assets/particles-config.json', function() {
-    console.log('particles.js loaded');
-});
-
-document.getElementById('subscribe-form').addEventListener('submit', function(event) {
+﻿document.getElementById('subscribe-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
     const messageElement = document.getElementById('subscription-message');
 
     if (email) {
         messageElement.textContent = `Thank you for subscribing, ${email}!`;
-        messageElement.style.color = '#ff79c6';
+        messageElement.style.color = '#ff4081';
         this.reset();
     } else {
         messageElement.textContent = 'Please enter a valid email address.';
